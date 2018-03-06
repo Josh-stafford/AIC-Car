11d296849ba6f337963bcea298f50e118700b5d6
Josh Stafford <joshstaffhome@gmail.com>
2018-03-06 17:38:33 +0000

    Created threads for send and receive

--- imgRecvr.py
+++ imgRecvr.py
@@ -1,5 +1,6 @@
 import socket
 from time import sleep
+import threading
 
 HOST = '192.168.1.251'
 PORT = int(input(">> "))
@@ -32,23 +33,34 @@ def process(arr):
     img = [ int(x) for x in arr ]
     asciiDisplay(img, 32, 32)
 
-while True:
-    data = s.recv(1)
-    count += 1
-    data = data.decode('utf-8')
-    if data == "*":
-        print("Start array")
-        recv_array = True
-        img_data = ""
-    elif data == ".":
-        print("End array")
-        recv_array = False
-        # print(img_data)
-        print(count)
-        count = 0
-        process(img_data.split(','))
-        #  asciiDisplay(img_data, 32, 32)
-    else:
-        # print(data)
-        img_data += data
-        # print(img_data)
+class recv(threading.Thread):
+
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+
+    def run(self):
+        while True:
+            data = s.recv(1)
+            count += 1
+            data = data.decode('utf-8')
+            if data == "*":
+                print("Start array")
+                recv_array = True
+                img_data = ""
+            elif data == ".":
+                print("End array")
+                recv_array = False
+                # print(img_data)
+                print(count)
+                count = 0
+                process(img_data.split(','))
+                #  asciiDisplay(img_data, 32, 32)
+            else:
+                # print(data)
+                img_data += data
+                # print(img_data)
+
+recvThread = recv(1, 'recvThread')
+recvThread.start()
--- imgSender.py
+++ imgSender.py
@@ -2,6 +2,7 @@ import socket, sys
 from time import sleep
 import imgProcessor
 import numpy as np
+import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,13 +20,24 @@ except:
 
 s.listen(10)
 
-while 1:
-    conn, addr = s.accept()
-    while sending:
-        img = imgProcessor.frame()
-        conn.send(bytes('*', 'utf-8'))
-        data = (','.join(str(x) for x in img.flatten().tolist()))
-        conn.send(bytes(data, 'utf-8'))
-        conn.send(bytes('.', 'utf-8'))
-        print('End')
-        sleep(0.2)
+class send(threading.Thread):
+
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+
+    def run(self):
+        while 1:
+            conn, addr = s.accept()
+            while sending:
+                img = imgProcessor.frame()
+                conn.send(bytes('*', 'utf-8'))
+                data = (','.join(str(x) for x in img.flatten().tolist()))
+                conn.send(bytes(data, 'utf-8'))
+                conn.send(bytes('.', 'utf-8'))
+                print('End')
+                sleep(0.2)
+
+sendThread = send(1, 'sendThread')
+sendThread.start()
